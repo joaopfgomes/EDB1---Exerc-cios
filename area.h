31256f81f37da615d933c27b7d224667415a0546
joaopfgomes <joaopaulofonsecagomes@gmail.com>
2024-10-01 18:33:42 -0300

    Atualizar o area.h

---
+++ area.h
@@ -0,0 +1,12 @@
+#ifndef AREA_H
+#define AREA_H
+
+#include "aresta.h"
+
+// Função para calcular a área de uma face do cubo
+float calcular_area_face(Cubo* c);
+
+// Função para calcular a área total das faces do cubo
+float calcular_area_total(Cubo* c);
+
+#endif // AREA_H
